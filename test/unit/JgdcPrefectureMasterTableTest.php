830f212e0da09c04bb32677eb19f1b20d7f9a94e
jiska <jiska.ym+github@gmail.com>
2011-10-17 13:10:30 +0900

    fixed bug: wrong configuration path...

--- test/unit/JgdcPrefectureMasterTableTest.php
+++ test/unit/JgdcPrefectureMasterTableTest.php
@@ -3,13 +3,12 @@ require_once __DIR__ . '/../bootstrap/Doctrine.php';
 
 $t = new lime_test();
 
-$t->is('北海道', JgdcPrefectureMasterTable::getNameByCode('01'));
-$t->is('東京都', JgdcPrefectureMasterTable::getNameByCode('13'));
-$t->is('沖縄県', JgdcPrefectureMasterTable::getNameByCode('47'));
-$t->is(false, JgdcPrefectureMasterTable::getNameByCode('1'));
-$t->is(false, JgdcPrefectureMasterTable::getNameByCode('0x01'));
-$t->is(false, JgdcPrefectureMasterTable::getNameByCode(48));
-
+$t->is(JgdcPrefectureMasterTable::getNameByCode('01'),   '北海道');
+$t->is(JgdcPrefectureMasterTable::getNameByCode('13'),   '東京都');
+$t->is(JgdcPrefectureMasterTable::getNameByCode('47'),   '沖縄県');
+$t->is(JgdcPrefectureMasterTable::getNameByCode('1'),    false);
+$t->is(JgdcPrefectureMasterTable::getNameByCode('0x01'), false);
+$t->is(JgdcPrefectureMasterTable::getNameByCode(48),     false);
 
 $expect = array(
   '01' => '北海道',
@@ -17,4 +16,4 @@ $expect = array(
   '47' => '沖縄県',
 );
 
-$t->is($expect, JgdcPrefectureMasterTable::getPrefectures());
+$t->is(JgdcPrefectureMasterTable::getPrefectures(), $expect);
